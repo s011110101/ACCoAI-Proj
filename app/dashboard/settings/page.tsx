80cc208a97fc3f4abab9d3a06544758f7967057c
s011110101 <59449094+s011110101@users.noreply.github.com>
2024-04-16 21:28:38 -0400

    initial webpage

---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; // Make sure React is imported
+import IndexComponent from '@/app/components';
+const SettingPage = () => {
+    return (
+        <div>
+            <IndexComponent/>
+        </div>
+    );
+};
+
+export default SettingPage;
